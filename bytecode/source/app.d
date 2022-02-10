00c8b2b36050bfce1d08c0e5df3043fe077cc5d6
João Lourenço <jlourenco5691@gmail.com>
2022-02-10 15:53:24 +0000

    test(bytecode): allow lox to run unit tests in betterC
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/app.d
+++ bytecode/source/app.d
@@ -2,5 +2,17 @@ module app;
 
 extern(C) int main(int argc, char** argv)
 {
+	version(dlox_unittest)
+	{
+		import core.stdc.stdio : printf;
+		import lox.dynamicarray;
+
+		static foreach (unit; __traits(getUnitTests, lox.dynamicarray))
+		{{
+			unit();
+			printf("%.*s -> passed\n", cast(int) __traits(identifier, unit).length, __traits(identifier, unit).ptr);
+		}}
+	}
+
 	return 0;
 }
