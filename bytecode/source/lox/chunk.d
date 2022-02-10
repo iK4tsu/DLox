f2f2a8daa95a32b92c8c1d9944862c5878ccabe3
João Lourenço <jlourenco5691@gmail.com>
2022-02-10 20:39:23 +0000

    feat(bytecode): chapter 14.4 - disassembling chunks
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/lox/chunk.d
+++ bytecode/source/lox/chunk.d
@@ -14,6 +14,100 @@ enum OpCode : ubyte
 struct Chunk
 {
 	import lox.dynamicarray;
+	import lox.util;
+	import std.sumtype;
+
+	/**
+	Convert machine instructions into human readable text.
+
+	Params:
+		name = chunk title.
+
+	Returns: a dynamic array with the disassembled information.
+	*/
+	@safe pure nothrow @nogc
+	scope DynamicArray!char disassembleChunk(string name = "Chunk")
+	{
+		import core.stdc.stdio : sprintf;
+
+		DynamicArray!char arr = "=== ";
+		arr.capacity = 256;
+		arr ~= name;
+		arr ~= " ===\n";
+
+		for (size_t i; i < code.length; i += instructionOffset(code[i]))
+		{
+			char[8] str;
+			() @trusted { assumePure(&sprintf)(str.ptr, "%04lu  | ", i); } ();
+			arr ~= str[];
+
+			disassembleInstruction(code[i]).match!(
+				(string s) { arr ~= s; },
+				(_) @trusted {
+					char[26] msg;
+					assumePure(&sprintf)(msg.ptr, "unkown instruction '%d'", i);
+					arr ~= msg[];
+				}
+			);
+
+			arr ~= '\n';
+		}
+
+		return arr;
+	}
+
+	/**
+	Instruction length. Each instruction must have a length with a minimum of 1 byte.
+	The length represents the offset to the next instruction. In between each
+	offset the index with the value correspondent to the instruction is stored.
+
+	Params:
+		op = OpCode to inspect.
+
+	Returns: the length of the instruction, an invalid instruction has length 1.
+	*/
+	@safe pure nothrow @nogc
+	size_t instructionOffset(in ubyte op) scope
+	{
+		switch (op) with (OpCode)
+		{
+			case return_:
+			default: return 1;
+		}
+	}
+
+	/**
+	Converts an instruction to it's human readable text representation.
+
+	Params:
+		op = OpCode to inspect.
+
+	Returns: an optional value containing the reprensation of a valid instruction
+	or nothing otherwise.
+	*/
+	@safe pure nothrow @nogc
+	Optional!string disassembleInstruction(in ubyte op) scope
+	{
+		import core.stdc.stdio : sprintf;
+
+		switch (op) with (OpCode)
+		{
+			case return_: return some("return");
+			default:      return none!string;
+		}
+	}
+
+	/*
+	Outputs the disassembled information of the chunk.
+	*/
+	@safe nothrow @nogc
+	void pprint() scope
+	{
+		auto str = disassembleChunk();
+		import core.stdc.stdio : printf;
+		() @trusted { printf("%.*s", cast(int) str.length, str[].ptr); } ();
+	}
+
 
 	/// bytecode is a series of instructions
 	DynamicArray!ubyte code;
