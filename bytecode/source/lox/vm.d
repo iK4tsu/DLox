1d377497779b78fa93436e0015e32a933766ed6c
João Lourenço <jlourenco5691@gmail.com>
2022-02-13 21:34:03 +0000

    feat(bytecode): chapter 15.2 - a value stack manipulator
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/lox/vm.d
+++ bytecode/source/lox/vm.d
@@ -15,6 +15,25 @@ struct VM
 	import lox.chunk;
 	import lox.value;
 
+	@disable this();
+
+	/// Correctly initializes the VM
+	@safe pure nothrow @nogc
+	static initialize()
+	{
+		VM vm = VM.init;
+		vm.initVM();
+		return vm;
+	}
+
+	/// Gets the VM in its initial state
+	@safe pure nothrow @nogc
+	void initVM() scope
+	{
+		// better than stack = stack.initialize()
+		resetStack();
+	}
+
 	/**
 	Interprets and runs a chunk of bytecode.
 
@@ -29,6 +48,13 @@ struct VM
 		this.chunk = &chunk;
 		this.ip = () @trusted { return &chunk.code[0]; } ();
 		return run();
+	}
+
+	/// Clears the VM's stack
+	@safe pure nothrow @nogc
+	void resetStack() scope
+	{
+		stack.reset();
 	}
 
 	/**
@@ -46,6 +72,16 @@ struct VM
 			version(debug_trace_execution)
 			debug {
 				import core.stdc.stdio : printf;
+
+				printf("%-10s", " ".ptr);
+				for (auto slot = &stack[0]; slot < stack.top; () @trusted { slot++; } ())
+				{
+					printf("[ ");
+					slot.print();
+					printf(" ]");
+				}
+				printf("\n");
+
 				const str = chunk.disassembleInstructionf(cast(size_t)(ip - &chunk.code[0]));
 				printf("%.*s", cast(int) str.length, str[].ptr);
 			}
@@ -54,13 +90,70 @@ struct VM
 			{
 				case constant: {
 					Value constant = readConstant();
+					stack.push(constant);
 					break;
 				}
-				case return_: return InterpretResult.ok;
+				case return_: {
+					stack.pop();
+					return InterpretResult.ok;
+				}
 			}
 		}
 	}
 
 	Chunk* chunk;
 	ubyte* ip;
+
+	Stack stack;
+}
+
+struct Stack
+{
+	import lox.value;
+
+	@disable this();
+
+	/// correctly initializes stack
+	@safe pure nothrow @nogc
+	static initialize()
+	{
+		Stack stack = Stack.init;
+		stack.reset();
+		return stack;
+	}
+
+	/**
+	Removes the element at the top of the stack
+
+	Returns: the removed element.
+	*/
+	@safe pure nothrow @nogc
+	Value pop() scope
+	{
+		return () @trusted { return *--top; } ();
+	}
+
+	/**
+	Inserts an element to the top of the stack.
+
+	Params:
+		value = element to add.
+	*/
+	@safe pure nothrow @nogc
+	void push(in Value value) scope
+	{
+		() @trusted {*top++ = value; } ();
+	}
+
+	/// Clears the stack
+	@safe pure nothrow @nogc
+	void reset() scope
+	{
+		top = () @trusted { return &stack[0]; } ();
+	}
+
+	enum stackmax = ubyte.max;
+	Value[stackmax] stack;
+	alias stack this;
+	Value* top;
 }
