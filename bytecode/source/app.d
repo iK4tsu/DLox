79ca04cbc532382142d6c9f7b34a859b96e8780d
João Lourenço <jlourenco5691@gmail.com>
2022-10-12 13:57:12 +0100

    feat(bytecode): chapter 14.4 - disassembling chunks
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/app.d
+++ bytecode/source/app.d
@@ -1,6 +1,14 @@
 module app;
 
+import chunk;
+import chunkdebug;
+
 void main(string[] args)
 {
+	Chunk chunk;
+	initChunk(chunk);
+	writeChunk(chunk, OpCode.opReturn);
 
+	disassembleChunk(chunk, "test chunk");
+	freeChunk(chunk);
 }
