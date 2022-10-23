1c589703d749aa32dea6867a0acee87a742f9ee3
João Lourenço <jlourenco5691@gmail.com>
2022-10-23 17:03:03 +0100

    feat(bytecode): chapter 15.2 - a value stack manipulator
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/vm.d
+++ bytecode/source/vm.d
@@ -3,6 +3,7 @@ module vm;
 import std.stdio : writeln;
 
 import chunk;
+import value;
 
 // VM being globaly defined is for easiness of the book
 VM vm;
@@ -11,12 +12,22 @@ struct VM
 {
 	Chunk* chunk;
 	ubyte* ip;
+	Value[256] stack;
+	Value* stackTop;
 }
 
 
-void initVM() {}
+void initVM()
+{
+	resetStack();
+}
 
 void freeVM() {}
+
+private void resetStack()
+{
+	vm.stackTop = &vm.stack[0];
+}
 
 
 
@@ -34,6 +45,18 @@ InterpretResult interpret(ref Chunk chunk)
 	return run();
 }
 
+void push(Value value)
+{
+	*vm.stackTop = value;
+	vm.stackTop++;
+}
+
+Value pop()
+{
+	vm.stackTop--;
+	return *vm.stackTop;
+}
+
 private InterpretResult run()
 {
 	auto readByte = () => *vm.ip++;
@@ -43,6 +66,9 @@ private InterpretResult run()
 	{
 		debug (debugTraceExecution)
 		{
+			import std.stdio : writefln;
+			writefln!"%10s%s"("", vm.stack[0 .. vm.stackTop - &vm.stack[0]]);
+
 			import chunkdebug;
 			disassembleInstruction(*vm.chunk, vm.ip - &vm.chunk.instructions[0]);
 		}
@@ -51,10 +77,11 @@ private InterpretResult run()
 		final switch (instruction)
 		{
 			case OpCode.opConstant:
-				readConstant().writeln();
+				readConstant().push();
 				break;
 
 			case OpCode.opReturn:
+				pop().writeln();
 				return InterpretResult.ok;
 		}
 	}
