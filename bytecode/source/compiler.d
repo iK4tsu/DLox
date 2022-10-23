c31b3b7a5239f7b8bae6e95e5ddd9be6fc5daf77
João Lourenço <jlourenco5691@gmail.com>
2022-10-23 18:35:39 +0100

    feat(bytecode): chapter 16.2 - a token at a time
    Signed-off-by: João Lourenço <jlourenco5691@gmail.com>

--- bytecode/source/compiler.d
+++ bytecode/source/compiler.d
@@ -6,4 +6,24 @@ import scanner;
 void compile(in string source)
 {
 	initScanner(source);
+	size_t line = 0;
+	while (true)
+	{
+		import std.stdio : write, writefln;
+
+		Token token = scanToken();
+		if (token.line != line)
+		{
+			writefln!"%04s"(token.line);
+			line = token.line;
+		}
+		else
+		{
+			write("   | ");
+		}
+
+		writefln!"%2s '%s'"(token.type, token.start[0 .. token.length]);
+
+		if (token.type == TokenType.eof) break;
+	}
 }
--- bytecode/source/scanner.d
+++ bytecode/source/scanner.d
@@ -7,12 +7,66 @@ struct Scanner
 {
 	immutable(char)* start;
 	immutable(char)* current;
+	immutable(char)* end;
 	size_t line;
+}
+
+struct Token
+{
+	TokenType type;
+	immutable(char)* start;
+	size_t length;
+	size_t line;
+}
+
+enum TokenType
+{
+	// single char tokens
+	left_paren, right_paren, left_brace, right_brace,
+	comma, dot, minus, plus, semicolon, slash, star,
+
+	// one or two char tokens
+	bang, bang_equal, equal, equal_equal,
+	greater, greater_equal, less, less_equal,
+
+	// literals
+	identifier, string_, number,
+
+	// keywords
+	and, class_, else_, false_, fun, for_, if_, nil, or,
+	print, return_, super_, this_, true_, var, while_,
+
+	error, eof,
 }
 
 void initScanner(const string source)
 {
 	scanner.start = &source[0];
 	scanner.current = &source[0];
+	scanner.end = &source[$ - 1] + 1; // points to address past the last
 	scanner.line = 1;
+}
+
+Token scanToken()
+{
+	scanner.start = scanner.current;
+
+	if (isAtEnd()) return makeToken(TokenType.eof);
+
+	return errorToken!"Unexpected character."();
+}
+
+private bool isAtEnd()
+{
+	return scanner.current == scanner.end;
+}
+
+private Token makeToken(TokenType type)
+{
+	return Token(type, scanner.start, scanner.current - scanner.start, scanner.line);
+}
+
+private Token errorToken(alias message)()
+{
+	return Token(TokenType.error, &message[0], message.length, scanner.line);
 }
